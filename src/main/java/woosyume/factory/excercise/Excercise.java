3c769d9dc7545bb047ddba0dd81f492e3b8c00e7
Aaron Kim <woohyeok.kim@rakuten.com>
2021-08-05 17:55:27 +0900

    Working with class, id

--- src/main/java/woosyume/factory/excercise/Excercise.java
+++ src/main/java/woosyume/factory/excercise/Excercise.java
@@ -12,5 +12,12 @@ public class Excercise {
     public static void main(String[] args) throws MalformedURLException {
         androidDriver = DriverFactory.getDriverFactory().getAndroidDriver();
         androidDriver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+        androidDriver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+
+        androidDriver.findElementById("android:id/checkbox").click();
+        androidDriver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+
+        androidDriver.findElementByClassName("android.widget.EditText").sendKeys("hello_woosyume");
+        androidDriver.findElementsByClassName("android.widget.Button").get(1).click();
     }
 }
