ed9d27556eee7783874e70c61511061c429a8b44
Aaron Kim <woohyeok.kim@rakuten.com>
2021-08-06 10:46:51 +0900

    Long Press

--- src/main/java/woosyume/factory/excercise/Gestures.java
+++ src/main/java/woosyume/factory/excercise/Gestures.java
@@ -1,17 +1,19 @@
 package woosyume.factory.excercise;
 
 import java.net.MalformedURLException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.LongPressOptions;
 import io.appium.java_client.touch.TapOptions;
 import io.appium.java_client.touch.offset.ElementOption;
-import io.appium.java_client.touch.offset.PointOption;
 import woosyume.factory.DriverFactory;
 
 public class Gestures {
@@ -27,5 +29,12 @@ public class Gestures {
 
         WebElement expandList = androidDriver.findElement(MobileBy.AndroidUIAutomator("text(\"Expandable Lists\")"));
         action.tap(TapOptions.tapOptions().withElement(ElementOption.element(expandList))).perform();
+        androidDriver.findElement(MobileBy.AndroidUIAutomator("text(\"1. Custom Adapter\")")).click();;
+
+        WebElement peopleName = androidDriver.findElement(MobileBy.AndroidUIAutomator("text(\"People Names\")"));
+        action.longPress(LongPressOptions.longPressOptions().withElement(ElementOption.element(peopleName)).withDuration(Duration.ofSeconds(2))).release().perform();
+
+        // androidDriver.findElement(By.id("android:id/title")).isDisplayed();
+        System.out.println("check=" + androidDriver.findElement(By.id("android:id/title")).isDisplayed());
     }
 }
